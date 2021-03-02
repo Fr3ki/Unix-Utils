850432c3805e50fc32064c91f708c61123099a90
Fr3ki <79277183+Fr3ki@users.noreply.github.com>
2021-03-02 20:49:13 +0000

    Documentation
    A quick update to add documentation.

--- USBUtility.sh
+++ USBUtility.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+#Simple function to make a devider between elements
 function devider() {
 
         echo "----------------------------------------------------------"
@@ -7,10 +7,22 @@ function devider() {
 }
 
 clear
+
+#Warning
+
 echo "WARNING: PLEASE CONSULT README.MD BEFORE USE"
+
 devider
+
+#Disclaimer
+
 echo "WARNING: Restore tool is not working completley as intended, use at your own risk"
+
+#Storing input as a variable "TOOL"
+
 read -p "Chose Functionality [ Flash | Restore ]: " TOOL
+
+#Bootable USB tool
 
 if [ $TOOL == "Flash" ]
 then
@@ -19,26 +31,53 @@ then
   ___           _        _    _       __  __        _ _         ___              _   _            _____         _ 
  | _ ) ___  ___| |_ __ _| |__| |___  |  \/  |___ __| (_)__ _   / __|_ _ ___ __ _| |_(_)___ _ _   |_   _|__  ___| |
  | _ \/ _ \/ _ \  _/ _  |  _ \ / -_) | |\/| / -_) _  | / _  | | (__|  _/ -_) _  |  _| / _ \   \    | |/ _ \/ _ \ |
- |___/\___/\___/\__\__,_|_.__/_\___| |_|  |_\___\__,_|_\__,_|  \___|_| \___\__,_|\__|_\___/_||_|   |_|\___/\___/_|
-                                                                                                                  "
-	#read -p "Enter the directory of your file: " PATH
- 	cd ~/
+ |___/\___/\___/\__\__,_|_.__/_\___| |_|  |_\___\__,_|_\__,_|  \___|_| \___\__,_|\__|_\___/_||_|   |_|\___/\___/_|"
+ 	
+	#Change directory to home and then to your Downloads/ISO folder
+	
+	cd ~/
 	cd Downloads/ISO
-	#cd /$PATH
+	
+	
 	devider
+	
+	#List all files in a single line
+	
 	ls -1
+	
 	devider
+	
+	#Store selection in var "ISO"
+	
 	read -p "Copy & paste an option: " ISO
+	
 	clear
+	
+	#Athstetic loading screen
+	
 	echo "GRABBING FILE..."
 	devider
 	sleep 1s
+	
+	#List drives
+	
 	lsblk
 	devider
+	
+	#Take drive selection and store it in var "DRIVE"
+	
 	read -p "Select drive [WARNING DO NOT CHOSE YOUR BOOT DRIVE] : " DRIVE 
 	clear
+	
+	#Warning #2
+	
 	echo "DO NOT CLOSE TERMINAL THIS NORMALLY TAKES A WHILE"
+	
+	#Take "DRIVE" and flash "ISO" once finished 
+	
 	sudo dd if=$ISO of=/dev/$DRIVE && clear &&  echo "Done! You may now close your cli."
+
+#Media Restoration tool
 
 elif [ $TOOL == "Restore" ]
 then
@@ -48,18 +87,34 @@ then
   __  __        _ _        ___        _                _   _            _____         _ 
  |  \/  |___ __| (_)__ _  | _ \___ __| |_ ___ _ _ __ _| |_(_)___ _ _   |_   _|__  ___| |
  | |\/| / -_) _  | / _  | |   / -_|_-<  _/ _ \ '_/ _  |  _| / _ \   \    | |/ _ \/ _ \ |
- |_|  |_\___\__,_|_\__,_| |_|_\___/__/\__\___/_| \__,_|\__|_\___/_||_|   |_|\___/\___/_| 
- "
+ |_|  |_\___\__,_|_\__,_| |_|_\___/__/\__\___/_| \__,_|\__|_\___/_||_|   |_|\___/\___/_| "
+	
+	#List Drives
+	
 	lsblk
  	devider
+	
+	#Save drive selection in var "SELECTION"
+	
 	read -p "Select drive [WARNING DO NOT CHOOSE YOUR BOOT DRIVE] : " SELECTION
 	clear
+	
+	#Warning
 	echo "WARNING THIS WILL ERASE DRIVE TO QUIT: Ctrl + C"
 	sleep 2s
+	
+	#Instructions
+	
 	echo "Select gpt label and create a new partiton, then write and quit"
 	sleep 10s
+	
+	#Restoration
 	sudo mkfs.ext4 /dev/$SELECTION && sudo cfdisk /dev/$SELECTION
+	#Done message
 	echo "Done! You may now use your device."
+
+
+##Usage tool UNDER CONSTRUCTION
 #elif [ $TOOL == "Usage" ]
 #then
 #	clear
